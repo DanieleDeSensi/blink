0f63b277d689e4f6d5980e27b1b08e1076839a46
Daniele De Sensi <d.desensi.software@gmail.com>
2024-03-08 10:20:01 +0100

    [FIX] Minor.

--- conf.sh
+++ conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export BLINK_SYSTEM="leonardo"
 export BLINK_ROOT=$(pwd)/
-export BLINK_GPU_MICROBENCH_COMMIT="f5aa9c2"
+export BLINK_GPU_MICROBENCH_COMMIT="f979a93"
 export BLINK_NCCL_TESTS_COMMIT="c6afef0"
 export BLINK_DNN_PROXIES_COMMIT="1d32dce"
 
