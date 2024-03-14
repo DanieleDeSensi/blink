43360023eb596cde6b2e47065812685afa54a0ab
Daniele  De Sensi <ddesensi@login02.leonardo.local>
2024-03-14 17:41:48 +0100

    [ADD] Leonardo new data.

--- wrappers/ib_send_bw.sh
+++ wrappers/ib_send_bw.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-IB_DEVICES=$1 # e.g. "mlx5_0|mlx5_1"
+IB_DEVICES=$1 # e.g. "mlx5_0#mlx5_1"
 ARGS=$2
 START_PORT=18515
 INDEX=0
-for DEV in $(echo $IB_DEVICES | sed "s/|/ /g")
+for DEV in $(echo $IB_DEVICES | sed "s/#/ /g")
 do
     CONN_PORT=$(( $START_PORT + $INDEX ))
     OUT_FILE=ib_send_bw_${DEV}.json
