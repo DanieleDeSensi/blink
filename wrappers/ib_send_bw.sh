c6e11fc6f9f596b6301a6c4998b51338fdc2bbaf
Daniele  De Sensi <ddesensi@login02.leonardo.local>
2024-03-14 13:33:11 +0100

    [ADD] Multiport ib_send_bw.

--- wrappers/ib_send_bw.sh
+++ wrappers/ib_send_bw.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 IB_DEVICES=$1 # e.g. "mlx5_0|mlx5_1"
 ARGS=$2
+START_PORT=18515
+INDEX=0
 for DEV in $(echo $IB_DEVICES | sed "s/|/ /g")
-do  
+do
+    CONN_PORT=$(( $START_PORT + $INDEX ))
     OUT_FILE=ib_send_bw_${DEV}.json
-    (rm -rf ib_send_bw_*.json; ib_send_bw --perform_warm_up --ib-dev=${DEV} --out_json --out_json_file=${OUT_FILE} --report_gbits -F ${ARGS}) &
+    (rm -rf ib_send_bw_*.json; ib_send_bw --perform_warm_up --ib-dev=${DEV} --out_json --out_json_file=${OUT_FILE} --report_gbits -F -p ${CONN_PORT} ${ARGS}) &
+    INDEX=$(( $INDEX + 1 ))
 done
 wait
