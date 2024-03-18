da1cdc0e54b3508f6ca5077fba3c9832403e0aed
Daniele De Sensi <d.desensi.software@gmail.com>
2024-03-18 15:58:40 +0100

    [FIX] Fixes for Alps.

--- conf.sh
+++ conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export BLINK_SYSTEM="lumi"
 export BLINK_ROOT=$(pwd)/
-export BLINK_GPU_MICROBENCH_COMMIT="0d07f33"
+export BLINK_GPU_MICROBENCH_COMMIT="ad40500"
 export BLINK_NCCL_TESTS_COMMIT="c6afef0"
 export BLINK_DNN_PROXIES_COMMIT="1d32dce"
 
