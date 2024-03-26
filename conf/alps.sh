444b9b48e567677df2ebc955272700dc2ba29f10
Luigi Fusco <luigifusco98@gmail.com>
2024-03-26 17:18:56 +0100

    alps data

--- conf/alps.sh
+++ conf/alps.sh
@@ -1,8 +1,12 @@
 # Modules load and any other needed command
 uenv start /bret/scratch/cscs/bcumming/images/prgenv-gnu-24.2-nccl.squashfs
 uenv view default
+source /bret/scratch/cscs/lfusco/spack/share/spack/setup-env.sh
+spack load python
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/user-environment/env/default/lib:/user-environment/env/default/lib64:/user-environment/linux-sles15-neoverse_v2/gcc-12.3.0/cray-gtl-8.1.28-zrpwudcmtmevtnjgtbug7ykbdyxkqmrn/lib
 export MPICH_SMP_SINGLE_COPY_MODE=CMA
 export MPICH_MALLOC_FALLBACK=1
+export NCCL_IGNORE_CPU_AFFINITY=1
 # Mandatory variables to compile/run microbenchmarks
 export BLINK_CC=mpicc # MPI C Compiler
 export BLINK_CXX=mpicxx # MPI C++ Compiler
@@ -16,7 +20,7 @@ export BLINK_MPIRUN="srun" # Command for running MPI applications
 export BLINK_MPIRUN_MAP_BY_NODE_FLAG="" # Flag to force ranks to be mapped by node (srun)
 export BLINK_MPIRUN_HOSTFILE_FLAG="" # Flag for specifying the hostfile
 export BLINK_MPIRUN_HOSTFILE_LONG_FLAG="" # Flag for specifying the hostfile (16 hosts)
-export BLINK_PINNING_FLAGS="--cpu-bind=map_cpu:1,73,145,217" #"--cpu-bind=socket" # Pinning flags
+export BLINK_PINNING_FLAGS="--cpu-bind=map_cpu=1,73,145,217" #"--cpu-bind=socket" # Pinning flags
 export BLINK_MPIRUN_ADDITIONAL_FLAGS=""    # Any additional flag that must be used by mpirun
 export BLINK_INTERFACE_MASK="148.187.36.181/19" # Interface address + mask size of the two nodes
 export BLINK_RUN_IB=false # Shall we run IB tests?
